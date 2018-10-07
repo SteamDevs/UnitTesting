0ca9c427f5c96f0263a90ad72ca237d1beb9a975
Pablo <pablojacobo1998@gmail.com>
2018-10-07 17:09:52 -0600

    Code Coverage

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## Para una prueba en especifico
 
 -- npm test <nombre_archivo>
+
+##Ejecutar el Coverage
+
+-- npm run test:coverage 
--- array.js
+++ array.js
@@ -1,3 +1,5 @@
 const lang = ['Python', 'Java', 'Dart', 'TypeScript','cobol']
+const framework = ['React', 'Angular', 'Laravel'];
 
 export const arrayLan = () => lang;
+export const arrayFrame = () => framework;
